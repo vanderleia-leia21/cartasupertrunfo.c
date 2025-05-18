8cf06ce322e38a051331ad38b9aefc36ae8902d8
vanderleia-leia21 <vanderleiasantosdaconceicaosan@gmail.com>
2025-05-18 01:04:36 +0000

    apaguei  scanf

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -10,13 +10,5 @@ float area;
 float PIB;
 int numero de pontos turisticos;
 
- printf("Bem vindo! \n Escolha uma carta: 01 ou 02");
-//cadrastro da primeiraa carta
-scanf("carta 1\n");
-scanf("estado: A01\n");
-scanf("");
-
-//cadrasto da segunda carta
-
 return 0;
 }
