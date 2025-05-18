6ed0a0e3644c58cf806d58253e181d259dd420c1
vanderleia-leia21 <vanderleiasantosdaconceicaosan@gmail.com>
2025-05-18 00:56:53 +0000

    criei o desafio super trunfo

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+printf("Desafio Super Trunfo -Países\n");
+int main(){
+char estado = Maralhão, Acre;
+char codigo da carta [10];
+char nome da cidade [20];
+int população;
+float area;
+float PIB;
+int numero de pontos turisticos;
+
+ printf("Bem vindo! \n Escolha uma carta: 01 ou 02");
+//cadrastro da primeiraa carta
+scanf("carta 1\n");
+scanf("estado: A01\n");
+scanf("");
+
+//cadrasto da segunda carta
+
+return 0;
+}
